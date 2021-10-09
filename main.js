a7e8ef8ad34d29e88fd7fcbb2ab6915bb5749908
M3L14m0Luc45 <lucasefabiani@gmail.com>
2021-10-08 23:45:53 -0300

    navbar fixed

---
+++ main.js
@@ -0,0 +1,15 @@
+var collapseBtn = document.getElementById('collapse-btn');
+var navbarCollapse = document.getElementById('nav-bar__collapse');
+let collapsed = true;
+
+collapseBtn.addEventListener("click",function(){
+	if(collapsed){
+	navbarCollapse.style.visibility = 'hidden';
+	navbarCollapse.style.height = '0';
+}else{
+	navbarCollapse.style.visibility = 'visible';
+	navbarCollapse.style.height = '300px';
+	}
+	collapsed = !collapsed
+
+})
